b02e0f6f4ef8800b5aa68197d8a255165d9f1a1d
Jens Egholm <jensegholm@protonmail.com>
2017-12-17 19:59:44 +0100

    readded heuristic tests

--- test/HeuristicSpec.hs
+++ test/HeuristicSpec.hs
@@ -18,11 +18,11 @@ spec = do
     it "should detect a red win in a column" $ do
       heuristic (boardFromList [[Just Red, Just Red], [Nothing, Nothing]]) `shouldBe` 100
 
-    -- it "should detect a blue win in a column" $ do
-    --   heuristic [[Just Blue, Just Blue], [Nothing, Nothing]]) `shouldBe` -100
-    --
-    -- it "should calculate a zero game" $ do
-    --   heuristic [[Nothing, Nothing], [Nothing, Nothing]] `shouldBe` 0
-    --
-    -- it "should give equal weight to red and blue" $ do
-    --   heuristic [[Just Red, Nothing], [Nothing, Just Blue]] `shouldBe` 0
+    it "should detect a blue win in a column" $ do
+      heuristic (boardFromList [[Just Blue, Just Blue], [Nothing, Nothing]]) `shouldBe` -100
+
+    it "should calculate a zero game" $ do
+      heuristic (boardFromList [[Nothing, Nothing], [Nothing, Nothing]]) `shouldBe` 0
+
+    it "should give equal weight to red and blue" $ do
+      heuristic (boardFromList [[Just Red, Nothing], [Nothing, Just Blue]]) `shouldBe` 0
